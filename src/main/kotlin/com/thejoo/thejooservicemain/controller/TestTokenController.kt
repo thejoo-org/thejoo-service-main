3c9aec2d1b7c1bbbad8f63287c2db8a144b955ea
ro.stradamus <rolee0429@gmail.com>
2022-02-03 22:57:24 +0900

    JOO-3: Configure sandbox environment

--- src/main/kotlin/com/thejoo/thejooservicemain/controller/TestTokenController.kt
+++ src/main/kotlin/com/thejoo/thejooservicemain/controller/TestTokenController.kt
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@Profile(value = ["dev", "test"])
+@Profile(value = ["dev", "test", "sandbox"])
 @RequestMapping("/api/test/token")
 @RestController
 class TestTokenController(
