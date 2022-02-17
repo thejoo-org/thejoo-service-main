d88cffc89c1a69fd45f1b39d58c0e6743a7d6f47
ro.stradamus <rolee0429@gmail.com>
2022-02-17 23:57:04 +0900

    JOO-33: [Backend] Fix lint

--- src/main/kotlin/com/thejoo/thejooservicemain/config/WebConfig.kt
+++ src/main/kotlin/com/thejoo/thejooservicemain/config/WebConfig.kt
@@ -5,9 +5,9 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-class WebConfig: WebMvcConfigurer {
+class WebConfig : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
         super.addCorsMappings(registry)
     }
 }
